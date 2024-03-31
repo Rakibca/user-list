6f2d7191851ae3095d029ce01bb6e5af79f785ed
Rakibul Islam <rakib.ca@gmail.com>
2024-03-31 12:59:33 -0600

    added dependency array

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import { useEffect, useState } from "react";
 import User from "./User";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch("users.json")
+    fetch("https://jsonplaceholder.typicode.com/users")
+      //fetch("users.json")
       .then((res) => res.json())
       .then(setUsers);
-  });
+  }, []);
 
   return (
     <>
       <h1>User List</h1>
       <ul>
-        {users.map(user => {
+        {users.map((user) => {
           return <User key={user.id} name={user.name} />;
         })}
       </ul>
