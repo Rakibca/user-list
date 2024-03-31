a115033b543fa97d2165bc0b60d275f985aff85d
Rakibul Islam <rakib.ca@gmail.com>
2024-03-31 13:17:23 -0600

    added isLoading state

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,35 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [users, setUsers] = useState([]);
 
+  const controller = new AbortController();
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
+    fetch("https://jsonplaceholder.typicode.com/users", {
+      signal: AbortController.signal,
+    })
       //fetch("users.json")
       .then((res) => res.json())
-      .then(setUsers);
+      .then(setUsers)
+      .finally(() => {
+        setIsLoading(false);
+      });
+
+    return () => {
+      controller.abort();
+    };
   }, []);
 
   return (
     <>
       <h1>User List</h1>
-      <ul>
-        {users.map((user) => {
-          return <User key={user.id} name={user.name} />;
-        })}
-      </ul>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <ul>
+          {users.map((user) => {
+            return <User key={user.id} name={user.name} />;
+          })}
+        </ul>
+      )}
     </>
   );
 }
